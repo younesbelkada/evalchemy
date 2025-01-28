43ff003f575454e40900718f667f9b13e442a453
Negin Raoof <neginmr@utexas.edu>
2025-01-28 04:02:16 -0600

    openai

--- eval/chat_benchmarks/AIME24/eval_instruct.py
+++ eval/chat_benchmarks/AIME24/eval_instruct.py
@@ -60,21 +60,38 @@ class AIME24Benchmark(BaseBenchmark):
         all_instances = []
         if isinstance(model, lm_eval.models.huggingface.HFLM):
             model_name = model.pretrained
+        elif isinstance(model, lm_eval.models.openai_completions.OpenAIChatCompletion):
+            model_name = str(f"openai/{model.model}")
         else:
             model_name = model.model_args["model"]
+        
         system_prompt = SYSTEM_PROMPT[model_name]
         for idx, example in enumerate(examples):
             messages = [
                 {"role": "system", "content": system_prompt},
                 {"role": "user", "content": PROMPT.format(problem=example["problem"])},
             ]
-            templated_messages = model.apply_chat_template(messages)
 
             generation_args = {
                 "do_sample": False,
-                "max_gen_toks" if isinstance(model, VLLM) else "max_new_tokens": self.max_new_tokens,
                 "temperature": 0.7,
             }
+            # Add max tokens except for OpenAI models
+            if not isinstance(model, lm_eval.models.openai_completions.OpenAIChatCompletion):
+                generation_args["max_gen_toks" if isinstance(model, VLLM) else "max_new_tokens"] = self.max_new_tokens
+            else:
+                if 'o1-mini' in model_name: # o1-mini is a special case for OpenAI models
+                    generation_args["max_tokens"] = 32768
+                    generation_args["temperature"] = 1
+                    messages = [
+                        {"role": "user", "content": system_prompt},
+                        {"role": "user", "content": PROMPT.format(problem=example["problem"])},
+                    ]
+                else:
+                    generation_args["max_tokens"] = 4096
+                    generation_args["temperature"] = 0.2
+
+            templated_messages = model.apply_chat_template(messages)
 
             all_instances.append(
                 Instance(
